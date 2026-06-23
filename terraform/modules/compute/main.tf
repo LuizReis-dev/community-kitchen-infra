732f19711a08bf559fece8e7ca845a686de065b4
Luiz Reis <reisluiz05@gmail.com>
2026-06-22 23:46:35 -0300

    fixing frontend deploy

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -20,6 +20,36 @@ resource "azurerm_public_ip" "ip" {
   sku                 = "Standard"
 }
 
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg-frontend"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-HTTP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
 resource "azurerm_network_interface" "nic" {
   name                = "nic-frontend"
   location            = var.location
@@ -31,6 +61,11 @@ resource "azurerm_network_interface" "nic" {
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.ip.id
   }
+}
+
+resource "azurerm_network_interface_security_group_association" "nic_nsg" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
